e4ddaa82f916404ad1507813f100f13f2633fd78
Sinje <sinje.balzer@web.de>
2021-02-27 16:45:12 +0100

    YabbaDabbaDo

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -1,10 +1,11 @@
 fun main() {
+    // YabbaDabbaDo
     var x = 1
-    println("Before the loop: x = $x")
-    while (x < 4) {
-        println("In the loop: x = $x")
+
+    while (x <3) {
+        print(if (x == 1) "Yab" else "Dab")
+        print("ba")
         x++
     }
-    println("After the loop: x = $x")
-
+    if(x == 3) println("Do")
 }
