3d256bde15b97ec7ff8527cb25822063732a2039
Sinje <sinje.balzer@web.de>
2021-02-27 16:33:57 +0100

    Some random variable, loops and prints

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -1,3 +1,16 @@
 fun main() {
     println("Pow!")
+
+    var x = 3
+    val name = "Sinje"
+    x *= 10
+    println("x is $x")
+
+    while (x > 20) {
+        x -= 1
+        println("x is $x")
+    }
+    if (name.equals("Sinje")) {
+        println("Yeah $name")
+    }
 }
