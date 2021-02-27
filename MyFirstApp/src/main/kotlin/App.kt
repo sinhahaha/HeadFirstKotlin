4561a78c2d74e68fc3bbc755e5ae6b692efbff10
Sinje <sinje.balzer@web.de>
2021-02-27 16:37:06 +0100

    Follow x in while loop

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -1,16 +1,10 @@
 fun main() {
-    println("Pow!")
-
-    var x = 3
-    val name = "Sinje"
-    x *= 10
-    println("x is $x")
-
-    while (x > 20) {
-        x -= 1
-        println("x is $x")
+    var x = 1
+    println("Before the loop: x = $x")
+    while (x < 4) {
+        println("In the loop: x = $x")
+        x++
     }
-    if (name.equals("Sinje")) {
-        println("Yeah $name")
-    }
+    println("After the loop: x = $x")
+
 }
